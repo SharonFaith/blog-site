77babcc38a59fa79d3a501044447cdbcff34b08d
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 16:40:22 +0300

    get secret key with os

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
 #   QUOTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 
-#    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://student:Amazeing16@localhost/blog_site'
     
