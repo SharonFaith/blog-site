8e80bb2005e21dc8707849879414cec8ab90bb7b
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 16:03:42 +0300

    add basic index view function

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ manager.add_command('server', Server)
 
 #@manager.command
 #def test():
-    """Run the unit tests."""
+#    """Run the unit tests."""
  #   import unittest
    # tests = unittest.TestLoader().discover('tests')
    # unittest.TextTestRunner(verbosity=2).run(tests)
