47e8477a3982e6aa4607f0af039ae6bc5a3c54ef
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 17:26:28 +0300

    add login form

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,16 @@ def index():
     '''
     return render_template('index.html')
 
+@main.route('/user/<uname>', methods = ['GET', 'POST'])
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+#    form = DeleteUser()
+ 
+    
+   
+    if user is None:
+        abort(404)
+    
+    else:
+
+        return render_template('profile/profile.html', user = user, delete = form, pitches = pitches)
