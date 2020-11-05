ee5dba3cfeff56c4743ace0d630c0eb4921fcd52
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 18:52:39 +0300

    add profile, update pic, update bio

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from . import main
 from flask_login import login_required, current_user
 from ..models import User
 from .. import db
+from .forms import UpdateProfile
 
 @main.route('/')
 def index():
@@ -24,3 +25,36 @@ def profile(uname):
     else:
 
         return render_template('profile/profile.html', user = user)
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    
+
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname = user.username))
+    
+    return render_template('profile/update.html', form = form)
+
+@main.route('/user/<uname>/update/pic', methods  = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname = uname))
