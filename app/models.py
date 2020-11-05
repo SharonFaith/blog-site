a73455783fd49ce81f6b01635d4b4a318704fd37
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 16:25:00 +0300

    add functions to set and hash password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 #id, username, email || bio, profilepicpath, blogs = db.relationship with blog class,
@@ -8,6 +8,20 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+
 
     def __repr__(self):
         return f'User {self.username}'
