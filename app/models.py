664d32892a36341ae7a2d05ed2bc8c32e994109f
SharonFaith <sharonfaith15@gmail.com>
2020-11-05 16:51:18 +0300

    add calleback function to retrieve user

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int( user_id))
+
 
 #id, username, email || bio, profilepicpath, blogs = db.relationship with blog class,
 
